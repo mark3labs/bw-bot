c86ef4d8a597f0ab8db78baff7620788e51932bb
Ed Zynda <ezynda3@gmail.com>
2022-02-18 13:41:53 +0300

    🔧 tweak selling of $MAGIC

--- src/extensions/magic.ts
+++ src/extensions/magic.ts
@@ -17,15 +17,34 @@ module.exports = (toolbox: GluegunToolbox) => {
         return
       }
       try {
-        let tx = await magicToken
-          .connect(recruit.wallet)
-          .approve(sushi.address, constants.MaxUint256)
-        await tx.wait()
+        let tx
+
+        // Check allowance
+        const allowance = await magicToken.allowance(
+          recruit.address,
+          sushi.address
+        )
+        if (allowance.lt(recruit.magicBalance)) {
+          tx = await magicToken
+            .connect(recruit.wallet)
+            .approve(sushi.address, constants.MaxUint256)
+          await tx.wait()
+        }
+
+        // Calculate slippage
+        const amountsOut = await sushi.getAmountsOut(utils.parseEther('10'), [
+          magicToken.address,
+          WETH_ADDRESS,
+        ])
+        const slippage = amountsOut[1].mul(5).div(1000) // 0.5% slippage
+        const amountOutMin = amountsOut[1].sub(slippage)
+
+        // Sell $MAGIC
         tx = await sushi
           .connect(recruit.wallet)
           .swapExactTokensForETH(
             recruit.magicBalance,
-            utils.parseEther('0.0001'),
+            amountOutMin,
             [magicToken.address, WETH_ADDRESS],
             recruit.address,
             moment().add(1, 'minute').unix()
--- src/lib/contracts.ts
+++ src/lib/contracts.ts
@@ -20,6 +20,7 @@ export const magicToken = new ethers.Contract(
   [
     'function balanceOf(address) external view returns (uint256)',
     'function approve(address,uint256) external',
+    'function allowance(address,address) external view returns (uint256)',
   ],
   provider
 )
